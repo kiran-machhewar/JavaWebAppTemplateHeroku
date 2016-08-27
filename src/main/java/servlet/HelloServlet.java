8354db51cfff10bcf05b079ab299ed74a37d0e29
shiva <smachhewar@gmail.com>
2016-08-27 12:36:11 +0530

    Added support for considering external jar.

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import myjarpackage.MyJarService;
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
@@ -19,7 +21,8 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku- JavaWebAppTemplateHeroku".getBytes());
+        String outputFromJar = MyJarService.getOutputFromJar();
+        out.write(("hello heroku- JavaWebAppTemplateHeroku\n "+outputFromJar).getBytes());
         out.flush();
         out.close();
     }
